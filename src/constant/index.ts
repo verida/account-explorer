a84449456871a0859069138c5a60ec621460593d
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-02-23 10:41:31 +0100

    fix: added css to fix box sizing #68

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,3 +1,4 @@
 export const USER_PROFILE = "PROFILE";
 
-export const BASIC_PROFILE_SCHEMA = "https://common.schemas.verida.io/social/contact/v0.1.0/schema.json";
+export const BASIC_PROFILE_SCHEMA =
+  "https://common.schemas.verida.io/social/contact/v0.1.0/schema.json";
