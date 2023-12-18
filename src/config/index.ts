4bfed5c50b589375e86f44dc5c496afcfcb2e90b
Chris <chris@verida.io>
2023-12-19 10:09:23 +1030

    Update code base to use new environment based config variables.

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,19 +2,26 @@ import { EnvironmentType } from "@verida/types";
 
 const nodeEnv = process.env.NODE_ENV || "development";
 
-const veridaEnv: EnvironmentType =
-  process.env.VUE_APP_VERIDA_ENV === "local"
-    ? EnvironmentType.LOCAL
-    : process.env.VUE_APP_VERIDA_ENV === "mainnet"
-    ? EnvironmentType.MAINNET
-    : EnvironmentType.TESTNET;
+console.log(process.env.VUE_APP_VERIDA_ENV);
+
+const veridaEnv: EnvironmentType = <EnvironmentType>(
+  process.env.VUE_APP_VERIDA_ENV
+);
 
 const veridaLogoUrl = process.env.VUE_APP_VERIDA_LOGO_URL;
 const veridaLoginText = process.env.VUE_APP_VERIDA_LOGO_URL;
 const veridaContextName = process.env.VUE_APP_VERIDA_CONTEXT_NAME;
 const veridaVaultContextName = process.env.VUE_APP_VERIDA_VAULT_CONTEXT_NAME;
+const veridaNodeSummaryUrl = process.env.VUE_APP_NODE_SUMMARY_URL;
+const veridaNetworkStatusUrl = process.env.VUE_APP_NETWORK_STATUS_URL;
 const veridaTestnetDefaultDidServerUrl =
   process.env.VUE_APP_VERIDA_TESTNET_DEFAULT_DID_SERVER;
+const veridaExplorerUrls = {
+  mainnet: process.env.VUE_APP_EXPLORER_URL_MAINNET,
+  testnet: process.env.VUE_APP_EXPLORER_URL_TESTNET,
+};
+
+console.log(veridaExplorerUrls);
 
 export const config = {
   nodeEnv,
@@ -24,4 +31,7 @@ export const config = {
   veridaContextName,
   veridaVaultContextName,
   veridaTestnetDefaultDidServerUrl,
+  veridaNodeSummaryUrl,
+  veridaNetworkStatusUrl,
+  veridaExplorerUrls,
 };
