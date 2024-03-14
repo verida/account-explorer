9c47c55e744525fc9c352e4dcd622c1856547e13
aurelticot <aurelticot.dev@gmail.com>
2024-03-14 11:17:50 +1100

    feat: add RPC env var in Verida config

--- src/@types/node/index.d.ts
+++ src/@types/node/index.d.ts
@@ -1,12 +1,12 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV?: "production" | "development" | "test";
+    VUE_APP_POLYGON_RPC_URL?: string;
     VUE_APP_VERIDA_CONTEXT_NAME?: string;
     VUE_APP_VERIDA_VAULT_CONTEXT_NAME?: string;
     VUE_APP_VERIDA_LOGO_URL?: string;
     VUE_APP_VERDIA_LOGIN_TEXT?: string;
     VUE_APP_BASE_URL?: string;
-    VUE_APP_VERIDA_TESTNET_DEFAULT_DID_SERVER?: string;
     VUE_APP_VERIDA_ENV?: "testnet" | "mainnet" | "local";
   }
 }
